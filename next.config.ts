e734904d03804794f67108a4b7cdc7dfcfcead88
chibuzor9 <emmanuelchibuzor2005@gmail.com>
2026-02-20 21:01:03 +0100

    fix: update output file tracing includes and postinstall script for Playwright

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	serverExternalPackages: ['playwright', 'playwright-core', '@axe-core/playwright'],
 	outputFileTracingIncludes: {
-		'/**': [
+        '/api/evaluate': [
 			'./node_modules/playwright-core/.local-browsers/**/*',
 		],
 	},
